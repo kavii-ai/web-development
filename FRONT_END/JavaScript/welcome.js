731a3ea4fcf3aa1c2b058c92675fc9a1dbcf1576
kavii-ai <85287346+kavii-ai@users.noreply.github.com>
2022-12-13 23:29:24 +0530

    welcome to js

---
+++ FRONT_END/JavaScript/welcome.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript</title>
+</head>
+<body>
+    <h1>Welcome to Javascript</h1>
+    <div id = " firstcontainer" class="container">
+        <button id = "click">click me</button>
+    </div>
+    <div class="container">
+        <p>this  is a paragraph</p>
+    </div>
+    <script>
+        console.log("this is script -blah bla-hello world");
+    </script>
+</body>
+</html>
